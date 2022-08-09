07b5d6d721e3b337f35020e08da33e20230261c7
rolia-net <48426314+rolia-net@users.noreply.github.com>
2022-08-09 11:16:22 -0700

    WritableBufferStream

--- index.js
+++ index.js
@@ -1,8 +1,24 @@
+const { Writable } = require('stream');
+
 module.exports = {
-	waitFor
+	waitFor,
+	WritableBufferStream
 };
 
 async function waitFor(job) {
 	while (job.done != true) { await new Promise(r => setTimeout(r, 100)); }
 }
 
+class WritableBufferStream extends Writable {
+	constructor(options) {
+		super(options);
+		this._chunks = [];
+	}
+	_write(chunk, enc, callback) {
+		this._chunks.push(chunk);
+		return callback(null);
+	}
+	toBuffer() {
+		return Buffer.concat(this._chunks);
+	}
+}
